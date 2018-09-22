15c8e1943aecded5e7c561b14fabeacfb910f3eb
Derry Nixon <nixondc93@gmail.com>
2018-09-22 09:17:05 -0700

    added function to wrap game

--- cardGame.cpp
+++ cardGame.cpp
@@ -12,7 +12,7 @@ void userPrompt(int arr[21]){
   cin.get();
 }
 
-void buildPiles(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7], int (&mainArr)[21]){
+void buildPiles(int firstArr[7], int secondArr[7], int thirdArr[7], int mainArr[21]){
   int count = 0;
   for(int i = 0; i < 21; i += 3){
     firstArr[count] = mainArr[i];
@@ -22,7 +22,7 @@ void buildPiles(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7], int
   }
 }
 
-int pilePrompt(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7]){
+int pilePrompt(int firstArr[7], int secondArr[7], int thirdArr[7]){
   int choice;
   for(int i = 0; i < 7; i++){
     cout << firstArr[i] << " ";
@@ -39,14 +39,10 @@ int pilePrompt(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7]){
   cout << "Which Pile is your number in? Enter 1, 2, or 3." << endl;
   cin >> choice;
 
-  // if(choice != 1 || choice != 2 || choice != 3){
-  //   cout << "Invalid entry. Please try again." << endl << endl;
-  //   pilePrompt(firstArr, secondArr, thirdArr);
-  // }
   return choice;
 }
 
-void shuffleMainArr(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7], int (&mainArr)[21], int choice){
+void shuffleMainArr(int firstArr[7], int secondArr[7], int thirdArr[7], int mainArr[21], int choice){
   if(choice == 1){
     int pileIdx = 0;
     for(int i = 0; i < 21; i++){
@@ -101,35 +97,29 @@ void shuffleMainArr(int (&firstArr)[7], int (&secondArr)[7], int (&thirdArr)[7],
   }
 }
 
-int findUserCard(int (&mainArr)[21]){
+int findUserCard(int mainArr[21]){
   return mainArr[10];
 }
 
-int main() {
+void numberGuessGame(){
   int mainArr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
   int firstArr[7], secondArr[7], thirdArr[7];
   int choice;
 
   userPrompt(mainArr);
 
-  buildPiles(firstArr, secondArr, thirdArr, mainArr);
-  choice = pilePrompt(firstArr, secondArr, thirdArr);
-  shuffleMainArr(firstArr, secondArr, thirdArr, mainArr, choice);
+  for(int i = 0; i < 3; i++){
+    buildPiles(firstArr, secondArr, thirdArr, mainArr);
+    choice = pilePrompt(firstArr, secondArr, thirdArr);
+    shuffleMainArr(firstArr, secondArr, thirdArr, mainArr, choice);
+  }
 
-  buildPiles(firstArr, secondArr, thirdArr, mainArr);
-  choice = pilePrompt(firstArr, secondArr, thirdArr);
-  shuffleMainArr(firstArr, secondArr, thirdArr, mainArr, choice);
+  cout << "\n\nIs this your number " << findUserCard(mainArr) << "?\n";
+}
 
-  buildPiles(firstArr, secondArr, thirdArr, mainArr);
-  choice = pilePrompt(firstArr, secondArr, thirdArr);
-  shuffleMainArr(firstArr, secondArr, thirdArr, mainArr, choice);
+int main() {
 
-  // for(int i = 0; i < 21; i++){
-  //   cout << mainArr[i] << "  ,";
-  // }
-
-  cout << endl << endl << "Your number: " << findUserCard(mainArr) << endl;
-
+  numberGuessGame();
 
   return 0;
 }
